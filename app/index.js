cefe278be9960e6bb0b659b0fefaf5e25017eba1
Ryan Barber <rhbarber@hotmail.com>
2020-11-19 16:27:08 -0500

    Updated splash styling and app frames
    Updated styling format for better readability and appearance.
    Updated launch scripts to set window title on page load
    Updated onClose event to exit application when all windows have been closed

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
-let { app, BrowserWindow } = require('electron')
-let mainWindow
+let { app, BrowserWindow } = require('electron'),
+    mainWindow,
+    splashWindow
 
 function createMainWindow () {
     mainWindow = new BrowserWindow({
@@ -10,29 +11,44 @@ function createMainWindow () {
             contextIsolation: true,
         },
         autoHideMenuBar: true,
-        title: 'CommCare for Windows Desktop',
         show: false
     })
     mainWindow.loadURL('https://commcarehq.org/accounts/login').then()
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.setTitle('CommCare Desktop Client (Version 0.3.0)')
+    })
 }
-
 function createSplashWindow () {
-    let splashWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
+    splashWindow = new BrowserWindow({
+        width: 600,
+        height: 200,
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
         },
-        autoHideMenuBar: true,
-        title: 'CommCare for Windows Desktop'
+        resizable: false,
+        frame: false
     })
     splashWindow.loadFile('./app/index.html').then(createMainWindow())
     mainWindow.on('ready-to-show', function() {
         mainWindow.show()
+        mainWindow.focus()
+        splashWindow.hide()
+        console.log('Application has finished loading.')
+        console.log('Exit State: 0')
+    })
+    mainWindow.on('close', function () {
         splashWindow.close()
     })
-
 }
-
 app.whenReady().then(createSplashWindow)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
